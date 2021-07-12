9cf19f6efa5a9c7aef3995351a7d7af9b2d44cb1
Liliangabe <87332499+Liliangabe@users.noreply.github.com>
2021-07-12 19:08:58 +0100

    Add files via upload

---
+++ OGUNDIMU ADEDOLAPO CRAMMER.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace crammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter values for a");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter values for b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter values for c");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter values for d");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter values for e");
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter values for f");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            double s = e*d - f*b;
+            double t = a*d - b*c;
+
+            double r = a*f - e*c;
+            double q = a*d - b*c;
+
+            double x = s/t;
+            double y = r/t;
+
+            
+
+            if (q == 0) 
+            {
+                System.Console.WriteLine("The equation has no solution");
+
+            }
+            else
+            {
+                Console.WriteLine("x is:"+x);
+                Console.WriteLine("y is:"+y);
+            }
+        }
+    }
+}
---
+++ OGUNDIMU ADEDOLAPO QUESTIONNAIRE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("are you full time or part time?");
+            string username = Convert.ToString(Console.ReadLine());
+
+            if ( username == "full time")
+            {
+                Console.WriteLine("what is your course of study");
+                string course = Convert.ToString(Console.ReadLine());
+                Console.WriteLine(course + " is a good course");
+            }
+            else if (username =="part time")
+            {
+                Console.WriteLine("how many credits he or she is taking");
+                int credit = Convert.ToInt32(Console.ReadLine());
+
+                if (credit > 6)
+                {
+                Console.WriteLine("too much unit for a part time student");
+                }
+
+                else if(credit <= 6)
+                {
+                Console.WriteLine("proceed to screen");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
